e7a4468048b0bcc362580a001096e3ad8774e24d
Keir Mierle <mierle@gmail.com>
2015-06-27 16:34:30 -0700

    Use argv for data source

--- munge_book_data_for_rnn.py
+++ munge_book_data_for_rnn.py
@@ -1,4 +1,5 @@
 import csv
+import sys
 
 # The Sci-Fi book metadata CSV file from Shelfie has the following columns:
 FIELDS = [
@@ -10,9 +11,13 @@ FIELDS = [
     'genre'
 ]
 
+if len(sys.argv) != 2:
+  sys.stdout.write('ERROR: missing metadata csv file argument')
+  sys.exit(1)
+
 # Format the output book title and author so that the neural network will learn
 # to output the generated books in a machine-parsable format.
-with open('../book_metadata.csv') as csvfile:
+with open(sys.argv[1]) as csvfile:
     reader = csv.DictReader(csvfile, fieldnames=FIELDS)
     for row in reader:
         print row['author'], '|||', row['title']
