4a4f1d3493f672f5df30e7286531d8f3a2b2caae
Keir Mierle <mierle@gmail.com>
2015-06-27 21:52:49 -0700

    Add more scripts for generating data

---
+++ data_generation/book-descriptions-selfhelp.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+th train.lua \
+-gpuid 0 \
+-eval_val_every 500 \
+-data_dir data/book-descriptions-selfhelp \
+-rnn_size 300 \
+-num_layers 3 \
+-dropout 0.3 \
+-seq_length 50 \
+-batch_size 25 \
+-savefile book-descriptions-selfhelp 
