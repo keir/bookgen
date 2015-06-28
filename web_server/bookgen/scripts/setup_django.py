563baa7f201f36e0000b4395203f6a7993c983c2
Amy Zhang <amy.xian.zhang@gmail.com>
2015-06-28 12:31:17 -0700

    script

---
+++ web_server/bookgen/scripts/setup_django.py
@@ -0,0 +1,8 @@
+import os
+import sys
+
+curdir, curfile = os.path.split(os.path.abspath(__file__))
+parentdir = os.path.normpath(os.path.join(curdir, '..'))
+sys.path.append(parentdir)
+
+os.environ["DJANGO_SETTINGS_MODULE"] = "bookgen.settings"
