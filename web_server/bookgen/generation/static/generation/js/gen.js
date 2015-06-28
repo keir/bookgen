62529c2c482abe57eaa5533ac3cd326c918b4197
Keir Mierle <mierle@gmail.com>
2015-06-28 14:08:06 -0700

    Tweaks

--- web_server/bookgen/generation/static/generation/js/gen.js
+++ web_server/bookgen/generation/static/generation/js/gen.js
@@ -11,6 +11,13 @@ function tripleToRGB(triple, opacity) {
                   triple[2] + ')';
 }
 
+function colorBrightness(triple) {
+  return triple[0] + triple[1] + triple[2];
+}
+
+function compareBrightness(a, b) {
+  return colorBrightness(a) - colorBrightness(b);
+}
 
 $(document).ready(function(){
         var colorThief = new ColorThief();
@@ -23,21 +30,42 @@ $(document).ready(function(){
 		var padding = Math.floor(Math.random() * (15));
 		$img.css({'padding': padding + 'px'});
 
-                var palette = colorThief.getPalette($img[0], 8)
-	
-		$('#title').css({'font-color': tripleToRGB(palette[0])});
-		$('#title').css({'background-color': tripleToRGB(palette[1])});
+                var palette = colorThief.getPalette($img[0], 16)
 
+                var sortedPalette = palette.sort(compareBrightness);
+
+                var titleColor = sortedPalette[0];
+                var titleBackgroundColor = sortedPalette[sortedPalette.length - 1];
+
+                var bookBackgroundColor = sortedPalette[0];
+                var authorColor = sortedPalette[14];
+                console.log(sortedPalette);
+
+                if (1 || Math.random() < 0.5) {
+                  titleBackgroundColor = bookBackgroundColor;
+                  titleColor = authorColor;
+                  titleColor = sortedPalette[12];
+                }
+
+                //titleColor = [0, 255, 0];
+	
+		$('#title').css({'color': tripleToRGB(titleColor)});
+		$('#title').css({'background-color': tripleToRGB(titleBackgroundColor)});
+
+		$('#book').css({'background-color': tripleToRGB(bookBackgroundColor)});
+
+		$('#author').css({'color': tripleToRGB(authorColor)});
 		
 		if (height > 550) {
 			var less = height - 100;
 			$('#title').css({'position': 'relative', 'top': '-' + less + 'px'});
 			$('#title').css({'background-color': '#ffffff'});
 			var opacity = (Math.random() * (1.0 - 0.50) + 0.50).toFixed(4);
+                        var opacity = 255;
 			$('#title').css({'background-color': 'rgba(255,255,255,' + opacity + ')'});
 
-                        $('#title').css({'font-color': tripleToRGB(palette[0])});
-                        $('#title').css({'background-color': tripleToRGB(palette[1], opacity)});
+                        $('#title').css({'color': tripleToRGB(titleColor)});
+                        $('#title').css({'background-color': tripleToRGB(titleBackgroundColor, opacity)});
 			
 			var border_thickness = Math.floor(Math.random() * (5));
 			var border_radius = Math.floor(Math.random() * (12));
@@ -47,8 +75,8 @@ $(document).ready(function(){
 			$('#title').css({'box-shadow': '0px 0px 0px ' + border_thickness + 'px rgba(0,0,0,' + opacity_border + ')'});
 			
 			$('#author').css({'position': 'relative', 'top': '-' + (less - 100) + 'px'});
-			$('#author').css({'background-color': '#ffffff'});
-			$('#author').css({'background-color': 'rgba(255,255,255,' + opacity + ')'});
+			//$('#author').css({'background-color': '#ffffff'});
+			//('#author').css({'background-color': 'rgba(255,255,255,' + opacity + ')'});
 
 			$('#book').css({'height': height + 14});
 		} else {
@@ -70,10 +98,12 @@ $(document).ready(function(){
 			
 		}
 		
+                /*
 		var div_height = $('#book').height();
-		if (div_height < 650) {
-			$('#book').css({'height': 650});
+		if (div_height < 450) {
+			$('#book').css({'height': 450});
 		} 
+                */
 		
 		if ($('#title').height() > 400) {
 			$('#title').css({'font-size': '60px'});
