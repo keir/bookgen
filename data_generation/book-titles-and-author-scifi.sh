126ce09f7b6ab865317ecc16113aba4f3d680991
Keir Mierle <mierle@gmail.com>
2015-06-27 23:02:09 -0700

    Add some generated scifi book author and titles

--- data_generation/book-titles-and-author-scifi.sh
+++ data_generation/book-titles-and-author-scifi.sh
@@ -4,9 +4,9 @@ th train.lua \
 -gpuid 0 \
 -eval_val_every 500 \
 -data_dir data/book-titles-and-author-scifi \
--rnn_size 202 \
+-rnn_size 300 \
 -num_layers 3 \
--dropout 0.5 \
--seq_length 75 \
+-dropout 0.3 \
+-seq_length 50 \
 -batch_size 25 \
 -savefile book-titles-and-author-scifi
